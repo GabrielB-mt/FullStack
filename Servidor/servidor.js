53a7d0fb802abcbf01fbb69a5511bda965fc932f
GabrielB-mt <gabrielbarbosanramos@gmail.com>
2025-04-29 16:52:57 -0300

    Aula 11

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -2,6 +2,17 @@ require("colors")
 var http = require('http');
 var express = require('express')
 let bodyParser = require("body-parser")
+var mongodb = require("mongodb")
+
+const MongoClient = mongodb.MongoClient;
+const uri = `mongodb+srv://GabrielB:gabM4242@fullstackbd.qgom4c8.mongodb.net/?retryWrites=true&w=majority&appName=FullStackBD`
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+
+var dbo = client.db("FullStackBD");
+var usuarios = dbo.collection("usuarios");
+var blog = dbo.collection("posts")
+
 
 
 var app = express();
@@ -10,6 +21,7 @@ app.use(bodyParser.urlencoded({extended: false }))
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 app.set('views', './views');
+
 
 var server = http.createServer(app);
 
@@ -24,10 +36,17 @@ app.get("/", function(requisicao, resposta){
     resposta.redirect("Laboratórios/Lab01/index.html")
 })
 
-
+app.get("/inicio", function(requisicao, resposta){
+    resposta.redirect("Laboratórios/Lab01/index.html")
+})
 app.post("/inicio", function(requisicao, resposta){
     resposta.redirect("Laboratórios/Lab01/index.html")
 })
+
+app.get("/Aula09", function(requisicao, resposta){
+    resposta.redirect("Aula09.html")
+})
+
 
 app.post("/cadastrar", function(requisicao, resposta){
     let nome = requisicao.body.nome;
@@ -36,8 +55,36 @@ app.post("/cadastrar", function(requisicao, resposta){
     let nasc = requisicao.body.nascimento;
     
     console.log(nome, login, senha, nasc)
-    resposta.render("resposta",{nome,login,senha,nasc});
+    var data = { db_nome: nome, db_login: login, db_senha: senha, db_nasc: nasc };
+    
+    usuarios.insertOne(data, function(err){
+        if(err){
+            resposta.render("resposta2",{status: "Erro", nome,login,senha,nasc})
+        }else{
+            resposta.render("resposta2",{status: "Sucesso",nome,login,senha,nasc})
+        }
+    })
 })
+
+app.post("/logar_usuario", function(requisicao, resposta) {
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+
+    var data = {db_login: login, db_senha: senha}
+
+    usuarios.find(data).toArray(function(err, items){
+        console.log(items)
+        if(items.length == 0){
+            resposta.render("loginresposta",{status: "Usuário/Senha não encontrado(s)"})
+        }else if(err){
+            resposta.render("loginresposta",{status: "Erro ao logar"})
+        }else{
+            resposta.render("loginresposta",{status: "Usuário "+login+" Logado"})
+        }
+    })
+
+})
+
 
 // app.get("/cadastrar", function(requisicao, resposta){
 //     let nome = requisicao.query.nome;
@@ -73,13 +120,8 @@ app.post("/cadastrolab", function(requisicao,resposta){
     conta.push(login)
     let senha = requisicao.body.senha
     conta.push(senha)
-    
-    console.log(conta)
-
     contas.push(conta)
 
-    console.log(contas)
-    
 
 })
 
@@ -89,4 +131,32 @@ app.post("/loginlab", function(requisicao,resposta){
     let senha = requisicao.body.senha2
 
     resposta.render("resposta",{login,senha,contas})
+})
+
+//------------------------------------------------------------------
+
+app.get("/blog", function(requisicao,resposta){
+    resposta.render("blog")
+})
+
+
+app.get("/postcriar", function(requisicao,resposta){
+    resposta.redirect("Laboratórios/Lab09/cadastrar_post.html")
+})
+
+
+app.post("/criacao", function(requisicao,resposta){
+    let titulo = requisicao.body.titulo
+    let resumo = requisicao.body.resumo
+    let conteudo = requisicao.body.conteudo
+    console.log(titulo,resumo,conteudo)
+
+    var data = {db_titulo: titulo, db_resumo: resumo, db_conteudo: conteudo}
+    blog.insertOne(data, function(err){
+        if(err){
+            resposta.render("blogresposta", :)
+        }else{
+            resposta.render("resposta2")
+        }
+    })
 })
